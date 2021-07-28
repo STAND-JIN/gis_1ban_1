5de37904cc9dc8e22aaafa4cf86d613c6164dc13
SEO JINHWAN <sjhge08@gmail.com>
2021-07-28 09:28:01 +0900

    7/28 1ban class commit 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    # update는 create와 달리 기존에 있던 것을 수정하는 것이기 때문에 바꾸고자 하는 대상을 지정해줘야 함 --> <int:pk>
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -16,3 +16,11 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    # 해당 내용을 접근할 때 사용
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('acoountapp:hello_world')
+    template_name = 'profileapp/update.html'
