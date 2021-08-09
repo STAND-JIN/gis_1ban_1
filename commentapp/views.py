0fb06d6175494e032d3fb5514ea0e50d353f6f5c
SEO JINHWAN <sjhge08@gmail.com>
2021-08-09 15:14:51 +0900

    8/9 1ban class commit 3

--- commentapp/views.py
+++ commentapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView
 
+from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -12,6 +13,13 @@ class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        # form.instance.article = Article.objects.get(pk=self.request.POST.get('article_pk'))
+        # DB에 article_id에 각 article마다 부여된 pk를 할당해서 나중에 특정 article을 찾을 때 사용함
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
