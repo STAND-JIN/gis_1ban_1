3b56a7f2bef7feec11841ed6604625e8889d456b
SEO JINHWAN <sjhge08@gmail.com>
2021-08-12 16:15:57 +0900

    8/12 1ban class commit 7

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 # 라우팅할 때 사용되는 경로 이름
 app_name = 'projectapp'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 
 ]
