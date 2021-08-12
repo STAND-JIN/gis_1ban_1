899598c1e6798b957ea6f426701c15e1941c8c6b
SEO JINHWAN <sjhge08@gmail.com>
2021-08-12 16:06:01 +0900

    8/12 1ban class commit 6

--- projectapp/models.py
+++ projectapp/models.py
@@ -8,7 +8,6 @@ class Project(models.Model):
     description = models.CharField(max_length=200, null=False)
     # article이나 profile처럼 media 디렉터리에 project 디렉를 생성하고 하위에 저장
     image = models.ImageField(upload_to='project/', null=False)
-
     created_at = models.DateTimeField(auto_now_add=True)
     # 작성받을 요소들 입력이 끝났으면 migration 해주기
     # 입력받을 form을 생성. projectapp/form.py 생성
--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
 
+from projectapp.views import ProjectCreateView
+
+# 라우팅할 때 사용되는 경로 이름
+app_name = 'projectapp'
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
 
 ]
