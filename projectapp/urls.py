b8ecc8aeeb539f728d78265d82cb75e43718d74f
SEO JINHWAN <sjhge08@gmail.com>
2021-08-18 09:24:03 +0900

    8/18 1ban class commit 2

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 # 라우팅할 때 사용되는 경로 이름
 app_name = 'projectapp'
@@ -8,5 +8,6 @@ app_name = 'projectapp'
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+    path('list/', ProjectListView.as_view(), name='list'),
 
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -19,7 +19,7 @@ class ProjectCreateView(CreateView):
     success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
     
-    # 게시판 생성에 성공하게 되면 해당 게시판으로 연결 
+    # 게시판 생성에 성공하게 되면 해당 게시판으로 연결
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
@@ -27,3 +27,10 @@ class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+
+class ProjectListView(ListView):
+    model = Project
+    # 프로젝트의 리스트를 담고 있어야 되기 때문에 다른 클래스와 달리 context_object_name이 'project_list'
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
