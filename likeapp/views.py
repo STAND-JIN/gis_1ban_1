35345f539e89e08d427e579bdb3124688a468c03
SEO JINHWAN <sjhge08@gmail.com>
2021-08-25 09:39:33 +0900

    8/25 1ban class commit 2

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -22,13 +23,16 @@ class LikeArticleView(RedirectView):
                                                article=article)
 
         if likeRecord.exists():
+            # 좋아요 반영되지 않음 -> ERROR 메세지 + 커스텀 문구
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
-
+        # 좋아요 반영됨 -> SUCCESS 메세지 + 커스텀 문구1
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
