16602117245ce9720fac74aa01890f4fb00edf21
SEO JINHWAN <sjhge08@gmail.com>
2021-08-26 15:05:16 +0900

    8/26 1ban class commit 1

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    # models.py에서는 models.~~였다면 forms.py에서는 forms.~~로 시작
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'text-align: left;'
+                                                                    'min-height: 10rem;'}))
+
     class Meta:
         model = Article
         # 클라이언트에게 입력받아야 될 요소
